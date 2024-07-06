75a564f0e1885bf096b25e6e3db470efba19ba39
LAUV\HP <phamnguyenvu2k5@gmail.com>
2024-07-06 23:54:15 +1000

    setting options for the form builder

--- public/index.js
+++ public/index.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    Formio.builder(document.getElementById('form-builder'),{},{
+    Formio.builder(document.getElementById('form-builder'), {}, {
         baseUrl: 'https://api.form.io',
         alwaysConfirmComponentRemoval: true,
-    }),then(function(builder){
+        display: 'form', // Specify the type of builder to be rendered: 'form', 'wizard', or 'pdf', ( default as form)
+        formConfig: {
+            premiumComponents: {
+                Tagpad: true, // Example of a premium component
+                Signature: true, // Enable Signature component
+                Sketchpad: true, // Enable Sketchpad component
+                Custom: true // Enable Custom component
+            }
+        }
+    }).then(function (builder) {
         console.log(builder.instance);
-    })
-})
+    }).catch(function (error) {
+        console.error('Form builder initialization failed:', error);
+    });
+});
