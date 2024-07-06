fbe978955d3acd8eb4e3fce71fdc6a7d5ddf1298
LAUV\HP <phamnguyenvu2k5@gmail.com>
2024-07-06 23:25:15 +1000

    initialize alwaysConfirmComponentRemoval option

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     Formio.builder(document.getElementById('form-builder'),{},{
-        baseUrl: 'https://api.form.io'
+        baseUrl: 'https://api.form.io',
+        alwaysConfirmComponentRemoval: true,
     }),then(function(builder){
         console.log(builder.instance);
     })
